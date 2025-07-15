8cde1346822e6286472cb8137b47bdf6922707f4
Percival <95fjpl95@gmail.com>
2025-07-15 06:33:18 -0600

    feat: all is left is adding remainign misteries GLORIOSOS LUMINOSOS. Static stuff is all set

--- app/Outro.jsx
+++ app/Outro.jsx
@@ -1,5 +1,5 @@
 import { Dialogus, Extra, Facio, Susurri, Dictum  } from './ui.jsx'
-import {padreNuestro, aveMaria, fe, esperanza, caridad, gloria, salve, letanias_1, letanias_2} from './prayers.js'
+import { padreNuestro, aveMaria, fe, esperanza, caridad, gloria, salve, letanias_1, letanias_2, letanias_3, letanias_4, letanias_final, oremos, aveMariaPurisima, jaculatorias_finales} from './prayers.js'
 
 export default function Outro (){
 
@@ -44,8 +44,30 @@ return <>
     <Extra titulo="Oración previa a Letanías" leyenda="¡Oh Señor! Ten misericordia de nosotros. Escucha nuestras súplicas." />
 
   <section className="bg-amber-100  px-4 py-2 text-amber-800 rounded-xl space-y-1 "  >
-      {letanias_1.map((letania, indx)=><Dictum key={indx} lider={letania.l} respuesta={letania.r} />)}
-      {letanias_2.map((letania, indx)=><Dictum key={indx} lider={letania.l} respuesta={letania.r} />)}
+
+        {letanias_1.map((letania, indx)=><Dialogus key={indx}><Dictum  lider={letania.l} respuesta={letania.r} /></Dialogus>)}
+                                                             
+        {letanias_2.map((letania, indx)=><Dialogus key={indx}><Dictum  lider={letania.l} respuesta={letania.r} /></Dialogus>)}
+                                                             
+        {letanias_3.map((letania, indx)=><Dialogus key={indx}><Dictum  lider={letania} respuesta="Ten piedad de nosotros." /></Dialogus>)}
+                                                              
+        {letanias_4.map((letania, indx)=><Dialogus key={indx}><Dictum lider={letania.l} respuesta={letania.r} /></Dialogus> )}
+
+      <Dialogus>
+        <Susurri lider={letanias_final.l} respuesta={letanias_final.r} />
+      </Dialogus>
+
+      <Dialogus>
+        <Facio> Oremos </Facio>
+        <Susurri lider={oremos.l} respuesta={oremos.r} />
+      </Dialogus>
+
+      
+        {aveMariaPurisima.map((letania, indx)=> <Dialogus key={indx}><Dictum lider={letania.l} respuesta={letania.r} /> </Dialogus> )}
+      
+
+      {jaculatorias_finales.map((letania, indx)=><Dialogus  key={indx}><Susurri lider={letania.l} respuesta={letania.r} /></Dialogus>)}
+
   </section>
   </>
 
--- app/prayers.js
+++ app/prayers.js
@@ -54,10 +54,96 @@ export const letanias_1 = [
 ]
 
 export const letanias_2 =  [
-{"l":"Dios Padre celestial, creador del mundo","r":"ten piedad de nosotros."},
-{"l":"Dios Hijo, Redentor del mundo,","r":"Ten piedad de nosotros."},
-{"l":"Dios, Espíritu Santo,", "r":"Ten piedad de nosotros."},
-{"l":"Santísima Trinidad, un solo Dios,", "r":"Ten misericordia de nosotros."}
+  {"l":"Dios Padre celestial, creador del mundo","r":"ten piedad de nosotros."},
+  {"l":"Dios Hijo, Redentor del mundo,","r":"Ten piedad de nosotros."},
+  {"l":"Dios, Espíritu Santo,", "r":"Ten piedad de nosotros."},
+  {"l":"Santísima Trinidad, un solo Dios,", "r":"Ten misericordia de nosotros."}
+]
+
+export const letanias_3 = [
+"Santa María, ruega por nosotros.",
+"Santa Madre de Dios,",
+"Santa Virgen de las Vírgenes,",
+"Madre de Cristo,",
+"Madre de la Iglesia,",
+"Madre de la misericordia,",
+"Madre de la divina gracia,",
+"Madre de la esperanza,",
+"Madre purísima,",
+"Madre castísima,",
+"Madre siempre virgen,",
+"Madre inmaculada,",
+"Madre amable,",
+"Madre admirable,",
+"Madre del buen consejo,",
+"Madre del Creador,",
+"Madre del Salvador,",
+"Virgen prudentísima,",
+"Virgen digna de veneración,",
+"Virgen digna de alabanza,",
+"Virgen poderosa,",
+"Virgen clemente,",
+"Virgen fiel,",
+"Espejo de justicia,",
+"Trono de la sabiduría,",
+"Causa de nuestra alegría,",
+"Vaso espiritual,",
+"Vaso digno de honor,",
+"Vaso de insigne devoción,",
+"Rosa mística,",
+"Torre de David,",
+"Torre de marfil,",
+"Casa de oro,",
+"Arca de la Alianza,",
+"Puerta del cielo,",
+"Estrella de la mañana,",
+"Salud de los enfermos,",
+"Refugio de los pecadores,",
+"Consuelo de los migrantes,",
+"Consoladora de los afligidos,",
+"Auxilio de los cristianos,",
+"Reina de los Ángeles,",
+"Reina de los Patriarcas,",
+"Reina de los Profetas,",
+"Reina de los Apóstoles,",
+"Reina de los Mártires,",
+"Reina de los Confesores,",
+"Reina de las Vírgenes,",
+"Reina de todos los Santos,",
+"Reina concebida sin pecado original,",
+"Reina asunta a los Cielos,",
+"Reina del Santísimo Rosario,",
+"Reina de la familia,",
+"Reina de la paz."
+]
+
+export const letanias_4 = [
+  {"l":"Cordero de Dios, que quitas el pecado del mundo,", "r":"Perdónanos, Señor."},
+  {"l":"Cordero de Dios, que quitas el pecado del mundo,", "r":"Escúchanos, Señor."},
+  {"l":"Cordero de Dios, que quitas el pecado del mundo,", "r":"Ten misericordia de nosotros y danos la paz."}
+]
+
+export const letanias_final = {
+"l": "Bajo tu amparo nos acogemos, Santa Madre de Dios. No desprecies nuestras súplicas en las necesidades que te presentamos. Antes bien, líbranos siempre de todos los peligros, Virgen gloriosa y bendita.  Ruega por nosotros, Santa Madre de Dios.",
+"r": "Para que seamos dignos de alcanzar las promesas y gracias de nuestro Señor Jesucristo. Amén."
+}
+
+export const oremos = {
+  "l": "Te rogamos señor, que nos concedas a nosotros tus siervos, gozar de perpetua  salud de alma y cuerpo, y por la gloriosa intercesión de la bienaventurada Virgen María, seamos librados de la tristeza presente y disfrutemos de la eterna alegría. Por Jesucristo, nuestro Señor.", "r":"Amén."
+}
+
+export const aveMariaPurisima =  [
+  {"l": "Ave Maria purísima" ,  "r":"sin pecado concebida"},
+  {"l": "Ave Maria purísima" ,  "r":"sin pecado concebida"},
+  {"l": "Ave Maria purísima" ,  "r":"sin pecado concebida"},
+]
+
+export const jaculatorias_finales = [
+  {"l":"Sagrado Corazón de Jesús,", "r":"en tí confío."},
+  {"l":"Dulce Corazón de María,", "r":"sed la salvación del alma mía. AMÉN"},
+  {"l":"En el nombre del Padre, del hijo y del Espíritu Santo.", "r": "AMÉN"},
+  {"l":"¡Que la paz del señor sea con nosotros,", "r": "y con su espíritu."},
+
 ]
 
 
