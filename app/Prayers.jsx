1f5bf30e525e3b8455119755f2e82a2f4bb0af00
Percival <95fjpl95@gmail.com>
2025-08-01 02:09:07 -0600

    feat: new approach. Carousel now requires Slides with a Frame and Steps. This way, the structure of Prayer component is recycled

--- app/Prayers.jsx
+++ app/Prayers.jsx
@@ -1,6 +1,6 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { Dialogus, Vox, Mysterium, Titulus, Up, Down, Add } from './ui.jsx' 
+import { Dialogus, Vox, Mysterium, Titulus, Up, Down, Left, Right, Add, Slide, Frame, Steps } from './ui.jsx' 
 import { padreNuestro, aveMaria, gloria, jaculatoria_1, jaculatoria_2 } from './prayers.js'
 import { useReducer, useRef } from 'react'
 import Beads from './Beads.jsx'
@@ -96,9 +96,9 @@ const singlePress = () => set({type: "next"})
   const bgColors = ['bg-violet-100', 'bg-purple-100', 'bg-fuchsia-100', 'bg-pink-100', 'bg-rose-100']
 
   return (
-    <div className={ `bg-gray-300 grid grid-cols-3 gap-0 relative overflow-hidden w-full flex-shrink-0` } >
+    <Slide>
 
-      <section className="col-span-3 relative w-full aspect-[812/899]">
+      <Frame>
         <Image
           className=""
           src={misterio.imagen}
@@ -107,61 +107,44 @@ const singlePress = () => set({type: "next"})
           sizes="(max-width: 768px) 100vw, 50vw"
           priority
         />
-      </section>
+      </Frame>
 
+      <Steps header={misterio.encabezado} up={goBack} down={singlePress} left={prev} right={next} >
 
-      <div className="col-span-3 grid grid-cols-3 gap-4">
+        <Prayer getter={state} setter={singlePress} title="Misterio" index={index} > 
+          <article className="pb-4 pt-4">
+            <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base font-bold">{misterio.titulo}</div>
+            <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base">Fruto del misterio: {misterio.fruto}</div>
+            <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base">{misterio.leyenda}</div>
+            <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base">{misterio.l}</div>
+            <div className=" bg-red-50 border-l-4  border-red-400 px-4 py-1  text-red-800 text-sm md:text-base">{misterio.r}</div>
+          </article>
+        </Prayer>
 
-      <section className="col-span-3 grid grid-cols-7 text-xs text-white/70 text-center bg-gray-800">
-        <button className="col-span-1" onClick={prev}><svg className="mx-auto"  xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#F3F3F3"><path d="m480-320 56-56-64-64h168v-80H472l64-64-56-56-160 160 160 160Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg></button>
-        <div className="col-span-5 h-10 flex items-center justify-center">{misterio.encabezado} </div>
-        <button className="col-span-1" onClick={next}>
-<svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#f3f3f3"><path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
-</button>
-      </section>
+        <Prayer getter={state} setter={singlePress} title="Padre Nuestro" index={index} > 
+          <Titulus>Padre Nuestro</Titulus>
+          <Vox lider={ padreNuestro.l } respuesta={ padreNuestro.r } />
+        </Prayer>
 
+        <Beads getter={state} setter={singlePress} index={index}>
+          <Titulus>Ave María (x10)</Titulus>
+          <Vox lider={ aveMaria.l } respuesta={ aveMaria.r } />
+        </Beads>
 
-        <div className="col-span-2 grid gap-2">
-          <Prayer getter={state} setter={singlePress} title="Misterio" index={index} > 
-            <article className="pb-4 pt-4">
-              <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base font-bold">{misterio.titulo}</div>
-              <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base">Fruto del misterio: {misterio.fruto}</div>
-              <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base">{misterio.leyenda}</div>
-              <div className="bg-teal-50 border-l-4 border-teal-400 px-4 py-1 text-teal-800 text-sm md:text-base">{misterio.l}</div>
-              <div className=" bg-red-50 border-l-4  border-red-400 px-4 py-1  text-red-800 text-sm md:text-base">{misterio.r}</div>
-            </article>
-          </Prayer>
+        <Prayer getter={state} setter={singlePress} title="Gloria" index={index}>
+          <Titulus>Gloria</Titulus>
+          <Vox lider={ gloria.l } respuesta={ gloria.r } />
+        </Prayer>
 
-          <Prayer getter={state} setter={singlePress} title="Padre Nuestro" index={index} > 
-            <Titulus>Padre Nuestro</Titulus>
-            <Vox lider={ padreNuestro.l } respuesta={ padreNuestro.r } />
-          </Prayer>
+        <Prayer getter={state} setter={singlePress} title="Jaculatorias" index={index}>
+          <Titulus>Jaculatorias</Titulus>
+          <Vox lider={jaculatoria_1.l} respuesta={jaculatoria_1.r} />
+          <Vox lider={jaculatoria_2.l} respuesta={jaculatoria_2.r} />
+        </Prayer>
 
-          <Beads getter={state} setter={singlePress} index={index}>
-            <Titulus>Ave María (x10)</Titulus>
-            <Vox lider={ aveMaria.l } respuesta={ aveMaria.r } />
-          </Beads>
+      </Steps>
 
-          <Prayer getter={state} setter={singlePress} title="Gloria" index={index}>
-            <Titulus>Gloria</Titulus>
-            <Vox lider={ gloria.l } respuesta={ gloria.r } />
-          </Prayer>
-
-          <Prayer getter={state} setter={singlePress} title="Jaculatorias" index={index}>
-            <Titulus>Jaculatorias</Titulus>
-            <Vox lider={jaculatoria_1.l} respuesta={jaculatoria_1.r} />
-            <Vox lider={jaculatoria_2.l} respuesta={jaculatoria_2.r} />
-          </Prayer>
-
-        </div>
-
-        <div className="col-span-1 grid ">
-          <Up onClick={goBack} />
-          <Down onClick={singlePress}/>
-        </div>
-      </div>
-
-    </div>
+    </Slide>
   )
 }
 
--- app/ui.jsx
+++ app/ui.jsx
@@ -80,6 +80,18 @@ export function Facio ({children}) {
 }
 
 
+export function Left ({onClick}){
+  return <button className="col-span-1" onClick={onClick}>
+    <svg className="mx-auto"  xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#F3F3F3"><path d="m480-320 56-56-64-64h168v-80H472l64-64-56-56-160 160 160 160Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+  </button>
+}
+
+export function Right ({onClick}){
+  return <button className="col-span-1" onClick={onClick}>
+    <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#f3f3f3"><path d="m480-320 160-160-160-160-56 56 64 64H320v80h168l-64 64 56 56Zm0 240q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+  </button>
+}
+
 
 export function Up ({ onClick }){
   return <button  onClick={onClick} className="flex items-center justify-center bg-gray-500 text-black rounded-tl-[35px]"> 
@@ -98,6 +110,41 @@ export function Add ({ onClick, name }){
   return <button onClick={onClick} name={name} className="py-2 col-span-1 bg-gray-600 text-white rounded text-sm">
     <svg className="mx-auto" xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#F3F3F3"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
   </button> 
+
+}
+
+
+export function Slide ({ children }){
+  return <div className={ `bg-gray-300 grid grid-cols-3 gap-0 relative overflow-hidden w-full flex-shrink-0` } >
+    {children}
+  </div>
+}
+
+export function Frame ({children}){
+  return <section className="col-span-3 relative w-full aspect-[812/899]">
+    {children}
+  </section>
+}
+
+export function Steps({children, header, up, down, left, right}) {
+  return <section className="col-span-3 grid grid-cols-3 gap-4">
+
+    <div className="col-span-3 grid grid-cols-7 text-xs text-white/70 text-center bg-gray-800">
+      <Left onClick={left}/>
+      <div className="col-span-5 h-10 flex items-center justify-center">{header} </div>
+      <Right onClick={right}/>
+    </div>
+
+    <div className="col-span-2 grid gap-2">
+      {children}
+    </div>
+
+    <div className="col-span-1 grid ">
+      <Up onClick={up} />
+      <Down onClick={down} />
+    </div>
+
+  </section>
 
 }
 
