32f567afc34e0da485f1e1bd098806be33e11ddc
Percival <95fjpl95@gmail.com>
2025-08-21 08:53:23 -0600

    feat: PWA works

---
+++ app/manifest.js
@@ -0,0 +1,23 @@
+export default function manifest() {
+  return {
+    name: 'El Santo Rosario',
+    short_name: 'Rosario',
+    description: 'Guia para rezar el Santo Rosario',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
