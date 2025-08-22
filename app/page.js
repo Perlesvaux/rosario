1bb5d3e40d950a65d28f2e91af2c869686842380
Percival <95fjpl95@gmail.com>
2025-08-22 00:27:12 -0600

    feat: sw added. It works, but requires visiting each endpoint.

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
 "use client"
+import { useEffect } from 'react'
 import Carousel from './Carousel.jsx'
 import { misterio_del_dia } from './prayers.js'
 import { useWakeLock } from './hooks.js'
 
+
+
 export default function Home() {
+useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/sw.js', { scope: '/' })
+        .then((registration) => console.log('scope is: ', registration.scope));
+    }
+  }, []);
 
   useWakeLock()
 
