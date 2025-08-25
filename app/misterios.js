c7c3685db49267d1d7d4ee7502dac9743f198fcb
Percival <95fjpl95@gmail.com>
2025-08-25 01:42:36 -0600

    fix: corrected mistaken gloriosos5.jpg image. Made all images imports instead of static paths

--- app/misterios.js
+++ app/misterios.js
@@ -4,6 +4,26 @@ import gozosos3 from '../public/gozosos3.jpg'
 import gozosos4 from '../public/gozosos4.jpg'
 import gozosos5 from '../public/gozosos5.jpg'
 
+import gloriosos1 from '../public/gloriosos1.jpg'
+import gloriosos2 from '../public/gloriosos2.jpg'
+import gloriosos3 from '../public/gloriosos3.jpg'
+import gloriosos4 from '../public/gloriosos4.jpg'
+import gloriosos5 from '../public/gloriosos5.jpg'
+
+import dolorosos1 from '../public/dolorosos1.jpg'
+import dolorosos2 from '../public/dolorosos2.jpg'
+import dolorosos3 from '../public/dolorosos3.jpg'
+import dolorosos4 from '../public/dolorosos4.jpg'
+import dolorosos5 from '../public/dolorosos5.jpg'
+
+import luminosos1 from '../public/luminosos1.jpg'
+import luminosos2 from '../public/luminosos2.jpg'
+import luminosos3 from '../public/luminosos3.jpg'
+import luminosos4 from '../public/luminosos4.jpg'
+import luminosos5 from '../public/luminosos5.jpg'
+
+
+
 export const gozosos = {
   nombre:"GOZOSOS",
   lista: [
@@ -61,7 +81,7 @@ export const dolorosos = {
   {
     encabezado: "DOLOROSO I - la oración en el huerto",   
     titulo: "EN EL PRIMER MISTERIO DOLOROSO: CONTEMPLAMOS LA ORACIÓN EN EL HUERTO",
-    imagen: "/dolorosos1.jpg",
+    imagen: dolorosos1,
     fruto: "¡Por el verdadero arrepentimiento de los pecados!",
     leyenda: "Y Jesús suplicaba  «Padre, si quieres, aparta de mí esta copa; pero no se haga mi voluntad, sino la tuya.» Y, sumido en agonía, insistía más en su oración. Sudaba como gotas espesas de sangre que caían en tierra. (Lucas 22: 39-46)",
     l: "¡Palabra del Señor,",
@@ -70,7 +90,7 @@ export const dolorosos = {
   {
     encabezado: "DOLOROSO II - La flagelación",   
     titulo: "EN EL SEGUNDO MISTERIO DOLOROSO:  CONTEMPLAMOS LA FLAGELACIÓN",
-    imagen: "/dolorosos2.jpg",
+    imagen: dolorosos2,
     fruto: "¡Por la virtud de la pureza!",
     leyenda: "Entonces, Pilato tomó a Jesús y mandó que fuera azotado. (Juan 19: 1)",
     l: "¡Palabra del Señor,",
@@ -79,7 +99,7 @@ export const dolorosos = {
   {
     encabezado: "DOLOROSO III - La coronación de espinas",   
     titulo: "EN EL TERCER MISTERIO DOLOROSO:  CONTEMPLAMOS LA CORONACIÓN DE ESPINAS",
-    imagen: "/dolorosos3.jpg",
+    imagen: dolorosos3,
     fruto: "¡Por el valor moral!",
     leyenda: "Lo desnudaron, lo vistieron con una túnica de púrpura, trenzaron una corona de espinas, y se la pusieron en la cabeza, y una caña en su mano derecha; y arrodillándose delante, se burlaban de Él diciendo: “¡Salve, Rey de los judíos!”. (Marcos 15: 16-20)",
     l: "¡Palabra del Señor,",
@@ -88,7 +108,7 @@ export const dolorosos = {
   {
     encabezado: "DOLOROSO IV - Jesús con la cruz a cuestas",   
     titulo: "EN EL CUARTO MISTERIO DOLOROSO:  CONTEMPLAMOS A JESÚS CON LA CRUZ A CUESTAS",
-    imagen: "/dolorosos4.jpg",
+    imagen: dolorosos4,
     fruto: "¡Por la virtud de la paciencia!",
     leyenda: "...Cargando Él la cruz, salió hacia el lugar llamado Calvario, en hebreo Gólgota. (Lucas 23: 26-32)",
     l: "¡Palabra del Señor,",
@@ -97,7 +117,7 @@ export const dolorosos = {
   {
     encabezado: "DOLOROSO V - La crucifixión",   
     titulo: "EN EL QUINTO MISTERIO DOLOROSO:   CONTEMPLAMOS LA CRUCIFIXIÓN",
-    imagen: "/dolorosos5.jpg",
+    imagen: dolorosos5,
     fruto: "¡Por la perseverancia final!",
     leyenda: "Y Jesús, dijo «Padre, perdónalos, porque no saben lo que hacen»... El cielo se oscureció, y Jesús, con su último aliento dijo: “Padre, en tus manos pongo mi espíritu”. Y habiendo dicho esto, expiró. (Lucas 23: 33-46)",
     l: "¡Palabra del Señor,",
@@ -114,7 +134,7 @@ export const gloriosos = {
   {
     encabezado: "GLORIOSO I - La resurrección",   
     titulo: "EN EL PRIMER MISTERIO GLORIOSO: CONTEMPLAMOS LA RESURRECCIÓN",
-    imagen: "/gloriosos1.jpg",
+    imagen: gloriosos1,
     fruto: "¡Por la virtud de la fe!",
     leyenda: "El Ángel se dirigió a las mujeres y les dijo: “No se turben. El que buscan, Jesús Nazareno, el crucificado, resucitó; no está aquí. Vean el lugar en que lo pusieron”. (Mateo 28: 5-7)",
     l: "¡Palabra del Señor,",
@@ -123,7 +143,7 @@ export const gloriosos = {
   {
     encabezado: "GLORIOSO II - La ascensión",   
     titulo: "EN EL SEGUNDO MISTERIO GLORIOSO:  CONTEMPLAMOS LA ASCENSIÓN",
-    imagen: "/gloriosos2.jpg",
+    imagen: gloriosos2,
     fruto: "¡Por la virtud de la esperanza!",
     leyenda: "El Señor Jesús, después de haber hablado con ellos, fue elevado al cielo, y se sentó a la diestra de Dios Padre. (San Marcos 16: 19)",
     l: "¡Palabra del Señor,",
@@ -132,7 +152,7 @@ export const gloriosos = {
   {
     encabezado: "GLORIOSO III - La venida del espíritu santo",   
     titulo: "EN EL TERCER MISTERIO GLORIOSO: CONTEMPLAMOS LA VENIDA DEL ESPÍRITU SANTO",
-    imagen: "/gloriosos3.jpg",
+    imagen: gloriosos3,
     fruto: "Por el amor hacia Dios.",
     leyenda: "...Se les aparecieron unas lenguas como de fuego que se repartieron y se posaron sobre cada uno de ellos; todos quedaron  llenos del Espíritu Santo, y comenzaron a hablar en otras lenguas, según el Espíritu les movía a expresarse. (Hechos 2:4)",
     l: "¡Palabra de Dios,",
@@ -141,18 +161,18 @@ export const gloriosos = {
   {
     encabezado: "GLORIOSO IV - La asunción de maría al cielo",   
     titulo: "EN EL CUARTO MISTERIO GLORIOSO: CONTEMPLAMOS LA ASUNCIÓN DE MARÍA AL CIELO",
-    imagen: "/gloriosos4.jpg",
+    imagen: gloriosos4,
     fruto: "¡Por la devoción a María!",
-    leyenda: "La santísima Virgen María, cumplido el curso de su vida terrena, fue llevada en cuerpo y alma a la gloria del cielo. Y se abrió el Santuario de Dios en el cielo, y apareció el arca de su alianza en el Santuario. (Apocalipsis 11: 19)",
+    leyenda: "La santísima Virgen María, cumplido el curso de su vida terrena, fue llevada en cuerpo y alma a la gloria del cielo. Y se abrió el Santuario de Dios en el cielo, y apareció el arca de su alianza en el Santuario. (Apocalipsis 11:19)",
     l: "¡Palabra de Dios,",
     r: "Te alabamos Señor!"
   },
   {
     encabezado: "GLORIOSO V - La coronación de la santa virgen maría",   
     titulo: "EN EL QUINTO MISTERIO GLORIOSO: CONTEMPLAMOS LA CORONACIÓN DE LA SANTA VIRGEN MARÍA",
-    imagen: "/gloriosos5.jpg",
+    imagen: gloriosos5,
     fruto: "¡Por la felicidad eterna!",
-    leyenda: "Una gran señal apareció en el cielo: Una mujer revestida de sol con la luna bajo sus pies y una corona de doce estrellas sobre su cabeza. (Eclesiástico 24: 22-28, 30-31)",
+    leyenda: "Una gran señal apareció en el cielo: Una mujer revestida de sol con la luna bajo sus pies y una corona de doce estrellas sobre su cabeza. (Apocalipsis 12:1)",
     l: "¡Palabra de Dios,",
     r: "Te alabamos Señor!"
   }
@@ -167,7 +187,7 @@ export const luminosos = {
   {
     encabezado: "LUMINOSO I - El bautismo de jesús",   
     titulo: "EN EL PRIMER MISTERIO LUMINOSO:  CONTEMPLAMOS EL BAUTISMO DE JESÚS",
-    imagen: "/luminosos1.jpg",
+    imagen: luminosos1,
     fruto: "Por la docilidad a la voluntad divina.",
     leyenda: "Una vez bautizado Jesús, salió del agua; y en esto se abrieron los cielos y se vio al Espíritu de Dios que bajaba en forma de paloma y venía sobre Él. Y una voz que provenía del cielo decía: “Éste es mi hijo amado, en quien me complazco”. (Mateo 3: 13-17)",
     l: "¡Palabra del Señor,",
@@ -176,7 +196,7 @@ export const luminosos = {
   {
     encabezado: "LUMINOSO II - La revelación de jesús en las bodas de caná",   
     titulo: "EN EL SEGUNDO MISTERIO LUMINOSO:  CONTEMPLAMOS LA REVELACIÓN DE JESÚS EN LAS BODAS DE CANÁ",
-    imagen: "/luminosos2.jpg",
+    imagen: luminosos2,
     fruto: "¡Por la devoción a María!",
     leyenda: "Le dice a Jesús su madre: “No tienen vino”… Dice su madre a los sirvientes: “Haced lo que Él os diga”. (Juan 2: 1-11)",
     l: "¡Palabra del Señor,",
@@ -185,7 +205,7 @@ export const luminosos = {
   {
     encabezado: "LUMINOSO III - El anuncio del reino de dios",   
     titulo: "EN EL TERCER MISTERIO LUMINOSO:  CONTEMPLAMOS EL ANUNCIO DEL REINO DE DIOS",
-    imagen: "/luminosos3.jpg",
+    imagen: luminosos3,
     fruto: "¡Por la gracia de la conversión!",
     leyenda: "Después que Juan fue entregado, marchó Jesús a Galilea; y proclamaba la Buena Nueva de Dios: “El tiempo se ha cumplido y el Reino de Dios está cerca; convertíos y creed en la Buena Nueva”. (Lucas 7: 36-50)",
     l: "¡Palabra del Señor,",
@@ -194,7 +214,7 @@ export const luminosos = {
   {
     encabezado: "LUMINOSO IV - Transfiguración de jesús",   
     titulo: "EN EL CUARTO MISTERIO LUMINOSO:  CONTEMPLAMOS LA TRANSFIGURACIÓN DE JESÚS",
-    imagen: "/luminosos4.jpg",
+    imagen: luminosos4,
     fruto: "¡Por el Santo temor de Dios!",
     leyenda: "Seis días después, toma Jesús consigo a Pedro, a Santiago y a su hermano Juan, y los lleva aparte, a un monte alto. Y se transfiguró delante de ellos: Su rostro se puso brillante como el sol y sus vestidos se volvieron blancos como la luz. (Mateo 17: 1-5)",
     l: "¡Palabra del Señor,",
@@ -203,7 +223,7 @@ export const luminosos = {
   {
     encabezado: "LUMINOSO V - La institución de la eucaristía",   
     titulo: "EN EL QUINTO MISTERIO LUMINOSO:  CONTEMPLAMOS LA INSTITUCIÓN DE LA EUCARISTÍA",
-    imagen: "/luminosos5.jpg",
+    imagen: luminosos5,
     fruto: "¡Por el agradecimiento a Dios!",
     leyenda: "Tomó luego pan, dio gracias, lo partió y se lo dio diciendo: “Este es mi cuerpo que se entrega por vosotros; haced esto en recuerdo mío”. (Mateo 26: 26-29)",
     l: "¡Palabra del Señor,",
