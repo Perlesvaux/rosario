e1dc7138786a69cb8312f658dbcf86c19f07c012
Percival <95fjpl95@gmail.com>
2026-04-02 23:12:20 -0600

    fix: added listener to activate event to clear old cache

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,104 @@
-const cacheName = 'v2'
+const cacheName = 'v1.7';
 
-const cacheClone = async (e) => {
-  const res = await fetch(e.request);
-  const resClone = res.clone();
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(cacheName).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
 
-  const cache = await caches.open(cacheName);
-  await cache.put(e.request, resClone);
-  return res;
-};
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(key => key !== cacheName).map(caches.delete))
+    )
+  );
+});
 
-const fetchEvent = () => {
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      cacheClone(e)
-        .catch(() => caches.match(e.request))
-        .then((res) => res)
-    );
-  });
-};
 
-fetchEvent();
+
+//const cacheName = 'v1.6'  // Just increment the version number
+//
+//const cacheClone = async (e) => {
+//  const res = await fetch(e.request);
+//  const resClone = res.clone();
+//
+//  const cache = await caches.open(cacheName);
+//  await cache.put(e.request, resClone);
+//  return res;
+//};
+//
+//const fetchEvent = () => {
+//  self.addEventListener('fetch', (e) => {
+//    e.respondWith(
+//      cacheClone(e)
+//        .catch(() => caches.match(e.request))
+//        .then((res) => res)
+//    );
+//  });
+//};
+//
+//fetchEvent();
+//
+//// Optional: Clean up old caches (add this)
+//self.addEventListener('activate', (e) => {
+//  e.waitUntil(
+//    caches.keys().then((keys) => {
+//      return Promise.all(
+//        keys.map((key) => {
+//          if (key !== cacheName) {
+//            return caches.delete(key);
+//          }
+//        })
+//      );
+//    })
+//  );
+//});
+
+
+
+//const cacheName = 'v1.6'
+//
+//const cacheClone = async (e) => {
+//  const res = await fetch(e.request);
+//  const resClone = res.clone();
+//
+//  const cache = await caches.open(cacheName);
+//  await cache.put(e.request, resClone);
+//  return res;
+//};
+//
+//
+//// Delete old caches on activation
+//const activateEvent = () => {
+//  self.addEventListener('activate', (e) => {
+//    e.waitUntil(
+//      caches.keys().then(keys => {
+//        return Promise.all(
+//          keys.filter(key => key !== cacheName)
+//              .map(key => caches.delete(key))
+//        );
+//      })
+//    );
+//  });
+//};
+//
+//
+//const fetchEvent = () => {
+//  self.addEventListener('fetch', (e) => {
+//    e.respondWith(
+//      cacheClone(e)
+//        .catch(() => caches.match(e.request))
+//        .then((res) => res)
+//    );
+//  });
+//};
+//
+//
+//activateEvent();
+//fetchEvent();
