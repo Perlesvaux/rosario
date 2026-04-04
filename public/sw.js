836b504a06e829265a7ad715a61f09dc1e0a6f8a
Percival <95fjpl95@gmail.com>
2026-04-04 15:12:18 -0600

    fix: now installation occurs immediately

--- public/sw.js
+++ public/sw.js
@@ -1,24 +1,54 @@
-const cacheName = 'v1.8';
+const cacheName = "v1.9";
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener("install", e => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== cacheName).map(k => caches.delete(k)))
+    )
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        const clone = res.clone();
-        caches.open(cacheName).then(cache => cache.put(e.request, clone));
+        if (res && res.ok) {
+          const clone = res.clone();
+          caches.open(cacheName).then(cache => cache.put(e.request, clone));
+        }
         return res;
       })
       .catch(() => caches.match(e.request))
   );
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => 
-      Promise.all(keys.filter(key => key !== cacheName).map(caches.delete))
-    )
-  );
-});
+//const cacheName = 'v1.8';
+//
+//self.addEventListener('fetch', (e) => {
+//  e.respondWith(
+//    fetch(e.request)
+//      .then(res => {
+//        const clone = res.clone();
+//        caches.open(cacheName).then(cache => cache.put(e.request, clone));
+//        return res;
+//      })
+//      .catch(() => caches.match(e.request))
+//  );
+//});
+//
+//self.addEventListener('activate', (e) => {
+//  e.waitUntil(
+//    caches.keys().then(keys => 
+//      Promise.all(keys.filter(key => key !== cacheName).map(caches.delete))
+//    )
+//  );
+//});
 
 
 
