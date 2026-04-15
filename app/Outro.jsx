7252e2daa9374b000bc55a7f97f7ed54b6ad607b
Percival <95fjpl95@gmail.com>
2026-04-15 17:33:44 -0600

    fix: Outro now works as expected

--- app/Outro.jsx
+++ app/Outro.jsx
@@ -21,62 +21,86 @@ function Outro ({header}){
         retrocede={prev}
       />
 
-      <Steps>
+      { !isSimple?
+        <Steps>
 
-        <Prayer title="Peticiones" to="peticiones">
-          <Introductio 
-            titulo="Peticiones"
-            leyenda="Peticiones. por los pedidos anteriores, por la iglesia, etc."
-          />
-        </Prayer>
-
-        <Prayer title="Padre Nuestro" to="padrenuestro">
-          <Dialogus 
-            titulo="Padre Nuestro"
-            lider={padreNuestro.l} 
-            respuesta={padreNuestro.r}
-          />
-        </Prayer>
-
-        <Prayer title="Fe, Esperanza y Caridad" to="avemarias">
-          <Dialogus titulo="Fe, Esperanza y Caridad"  >
-            <div className="px-4" >
-            <Dialogus
-              titulo="Ave María por la Fe"
-              lider={fe} 
-              respuesta={aveMaria.r}
+          <Prayer title="Peticiones" to="peticiones">
+            <Introductio 
+              titulo="Peticiones"
+              leyenda="Peticiones. por los pedidos anteriores, por la iglesia, etc."
             />
-            <Dialogus
-              titulo="Ave María por la Esperanza"
-              lider={esperanza} 
-              respuesta={aveMaria.r}
-            />
-            <Dialogus
-              titulo="Ave María por la Caridad"
-              lider={caridad} 
-              respuesta={aveMaria.r}
-            />
-            </div>
-          </Dialogus>
-        </Prayer>
+          </Prayer>
 
-        <Prayer title="Gloria" to="gloria">
-          <Dialogus
-            titulo="Gloria"
-            lider={gloria.l} 
-            respuesta={gloria.r}
-          />
-        </Prayer>
+          <Prayer title="Padre Nuestro" to="padrenuestro">
+            <Dialogus 
+              titulo="Padre Nuestro"
+              lider={padreNuestro.l} 
+              respuesta={padreNuestro.r}
+            />
+          </Prayer>
 
-        <Prayer title="Salve" to="salve">
-          <Dialogus
-            titulo="Salve" 
-            lider={salve.l} 
-            respuesta={salve.r}
-          />
+          <Prayer title="Fe, Esperanza y Caridad" to="avemarias">
+            <Dialogus titulo="Fe, Esperanza y Caridad"  >
+              <div className="px-4" >
+                <Dialogus
+                  titulo="Ave María por la Fe"
+                  lider={fe} 
+                  respuesta={aveMaria.r}
+                />
+                <Dialogus
+                  titulo="Ave María por la Esperanza"
+                  lider={esperanza} 
+                  respuesta={aveMaria.r}
+                />
+                <Dialogus
+                  titulo="Ave María por la Caridad"
+                  lider={caridad} 
+                  respuesta={aveMaria.r}
+                />
+              </div>
+            </Dialogus>
+          </Prayer>
+
+          <Prayer title="Gloria" to="gloria">
+            <Dialogus
+              titulo="Gloria"
+              lider={gloria.l} 
+              respuesta={gloria.r}
+            />
+          </Prayer>
+
+          <Prayer title="Salve" to="salve">
+            <Dialogus
+              titulo="Salve" 
+              lider={salve.l} 
+              respuesta={salve.r}
+            />
+          </Prayer>
+
+        </Steps>
+
+          :
+          <Steps>
+
+          <Prayer title="Salve" to="salve">
+            <Dialogus
+              titulo="Salve" 
+              lider={salve.l} 
+              respuesta={salve.r}
+            />
+          </Prayer>
+
+          <Prayer title="Final" to="final">
+            <Dialogus titulo="Final">
+              <div className="flex flex-col gap-1">
+                {jaculatorias_finales.map((letania, indx)=><div  key={indx}><Vox lider={letania.l} respuesta={letania.r} /></div>)}
+              </div>
+            </Dialogus>
         </Prayer>
 
       </Steps>
+      }
+
 
     </Slide>
   </PrayerContext.Provider>)
